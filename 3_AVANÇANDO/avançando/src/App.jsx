7d4a4a0db6c5f7a86ffb3e3e241ded9a3545f8c5
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-02-04 19:54:04 -0300

    .

---
+++ 3_AVANÇANDO/avançando/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+ {/* {imagem com assets} */}
+import motor from "./assets/img2.jpg";
+function App() {
+  
+
+  return (
+    <div className='app'>
+      <h1>avançando com react</h1>
+      {/* {imagem em public} */}
+      <img src="/img.jpg" alt="imagem public" />
+
+      {/* {imagem com assets} */}
+      <img src={motor} alt="motor " />
+    </div>
+  )
+}
+
+export default App
