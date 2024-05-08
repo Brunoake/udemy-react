a91aa499281849e6809194323493345799cb7827
Brunoake <brunopachecogre@gmail.com>
2024-05-08 13:31:01 -0300

    Update HookUseEffect.jsx

--- 9_HOOKS/react_hooks/src/components/HookUseEffect.jsx
+++ 9_HOOKS/react_hooks/src/components/HookUseEffect.jsx
@@ -9,11 +9,33 @@ const [number, setNumber] = useState(3)
 const changeSomething = () => {
   setNumber (number + 1);
 }
+
+// ARRAY VAZIO
+
+useEffect(() =>{
+  console.log("Serei executado apenas uma vez")
+}, [])
+
+// ARRAY DE DEPENDENCIA COM ALGO
+
+const [anotherNumber, setAnotherNumber] = useState(0)
+
+useEffect(() => {
+
+  if(anotherNumber > 0) (
+    console.log("Sou executado apenas quando muda o anotherNumber")
+  )
+
+}, [anotherNumber])
+
   return (
     <div>
       <h2>use Effect</h2> 
       <p>Number: {number}</p> 
       <button onClick={changeSomething}>Executar</button>
+
+      <p>Another Number {anotherNumber}</p>
+      <button onClick={() => setAnotherNumber(anotherNumber + 1)}>Mudar anotherNumber</button>
         </div>
   )
 }
